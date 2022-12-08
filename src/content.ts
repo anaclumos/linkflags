0329a221cacb7f2817604dcf8401c058891e5109
Sunghyun Cho <mail@chosunghyun.com>
2022-12-07 23:07:03 -0800

    finish v1

--- src/content.ts
+++ src/content.ts
@@ -1,10 +1,15 @@
 import styles from './styles.css'
 
-const link = document.getElementsByTagName('a')[0]
-if (link) {
-  const color = window.getComputedStyle(link).color
-  document.documentElement.style.setProperty('--linkflags-color', color)
+const setLinkflagsColor = () => {
+  const links = document.getElementsByTagName('a')
+  const link = links[Math.floor(links.length / 2)]
+  if (link) {
+    const color = window.getComputedStyle(link).color
+    document.documentElement.style.setProperty('--linkflags-color', color)
+  }
 }
+
+setTimeout(setLinkflagsColor, 1000)
 
 const style = document.createElement('style')
 style.textContent = styles
