bc704d63b77c35535af4268be2aa2b54e7098841
Sunghyun Cho <mail@chosunghyun.com>
2022-12-08 17:15:03 -0800

    optimize build

--- src/content.ts
+++ src/content.ts
@@ -1,16 +1,8 @@
-import styles from './styles.css'
-
-const setLinkflagsColor = () => {
-  const links = document.getElementsByTagName('a')
-  const link = links[Math.floor(links.length / 2)]
-  if (link) {
-    const color = window.getComputedStyle(link).color
-    document.documentElement.style.setProperty('--linkflags-color', color)
-  }
+const inject = () => {
+  let styles: string = require('./index.css')
+  const style = document.createElement('style')
+  style.textContent = styles
+  document.head.appendChild(style)
 }
 
-setTimeout(setLinkflagsColor, 1000)
-
-const style = document.createElement('style')
-style.textContent = styles
-document.head.appendChild(style)
+inject()
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     content: path.join(srcDir, 'content.ts'),
   },
   output: {
-    path: path.join(__dirname, '../dist'),
+    path: path.join(__dirname, '../release'),
     filename: '[name].js',
   },
   optimization: {
